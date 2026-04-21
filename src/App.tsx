13c9429c136bf9dba8c562e2034baf69daa9d813
alex <343072005@tdsb.ca>
2026-04-21 10:08:17 -0400

    copied mr q router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { MainLayout } from '@/layouts/MainLayout';
+import { CreditsView, ErrorView, HomeView, MovieView, NowPlayingView, ReviewsView, SearchView, TrendingView } from '@/views';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+export const App = () => {
   return (
-    <>
-    </>
-  )
-}
-
-export default App
+    <Routes>
+      <Route path="/" element={<HomeView />} />
+      <Route element={<MainLayout />}>
+        <Route path="/now-playing" element={<NowPlayingView />} />
+        <Route path="/trending" element={<TrendingView />} />
+        <Route path="/search" element={<SearchView />} />
+        <Route path="/movie/:id" element={<MovieView />}>
+          <Route path="credits" element={<CreditsView />} />
+          <Route path="reviews" element={<ReviewsView />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<ErrorView />} />
+    </Routes>
+  );
+};
