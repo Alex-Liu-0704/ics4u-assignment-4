f0beb0bbb2c8ea08fdb308bc1c643b59e46c9b26
alex <343072005@tdsb.ca>
2026-04-22 10:54:50 -0400

    started genre view

---
+++ src/views/movies/GenresView.tsx
@@ -0,0 +1,31 @@
+import { GENRE_ENDPOINT } from "@core/constants";
+
+const MOVIE_GENRES = [
+    { name: "Action", id: 28 },
+    { name: "Adventure", id: 12 },
+    { name: "Animation", id: 16 },
+    { name: "Crime", id: 80 },
+    { name: "Family", id: 10751 },
+    { name: "Fantasy", id: 14 },
+    { name: "History", id: 36 },
+    { name: "Horror", id: 27 },
+    { name: "Mystery", id: 9648 },
+    { name: "Sci-Fi", id: 878 }
+];
+
+const TV_GENRES = [
+    { name: "Action", id: 10759 },
+    { name: "Animation", id: 16 },
+    { name: "Comedy", id: 35 },
+    { name: "Crime", id: 80 },
+    { name: "Documentary", id: 99 },
+    { name: "Drama", id: 18 },
+    { name: "Family", id: 10751 },
+    { name: "Kids", id: 10762 },
+    { name: "Mystery", id: 9648 },
+    { name: "Sci-Fi", id: 10765 }
+];
+
+export const GenreView = () => {
+    return <></>
+};
