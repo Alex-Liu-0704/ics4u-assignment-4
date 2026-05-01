4c89918beb3984d5b297147ff5ad1c22a67674a2
Alex Liu <xixiliu0704@gmail.com>
2026-04-30 22:52:19 -0400

    finished moviesview

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { MainLayout } from '@/layouts/MainLayout';
-import { CreditsView, ErrorView, HomeView, MovieView, NowPlayingView, ReviewsView, SearchView, TrendingView } from '@/views';
-import { Route, Routes } from 'react-router-dom';
+import { CreditsView, ErrorView, HomeView, MovieView, MoviesView, ReviewsView, SearchView, TrendingView } from '@/views';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomeView />} />
       <Route element={<MainLayout />}>
-        <Route path="/now-playing" element={<NowPlayingView />} />
+        <Route path="/movies" element={<Navigate to="/movies/category/now_playing" replace />} />
+        <Route path="/movies/category/:category" element={<MoviesView />} />
         <Route path="/trending" element={<TrendingView />} />
         <Route path="/search" element={<SearchView />} />
         <Route path="/movie/:id" element={<MovieView />}>
--- src/views/movies/MoviesView.tsx
+++ src/views/movies/MoviesView.tsx
@@ -1,37 +1,41 @@
-// originally was now playing view, currently editing to make movies view and can change between the 4 endpoints
 import { ImageGrid, LinkGroup, Pagination } from '@/components';
-import { MOVIE_ENDPOINT, NOW_PLAYING_ENDPOINT } from '@/core/constants';
+import { MOVIE_ENDPOINT } from '@/core/constants';
 import type { MoviesResponse } from '@/core/types';
 import { useTmdb } from '@/hooks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-
-export const NowPlayingView = () => {
-  const { endpoint } = useParams();
+ 
+export const MoviesView = () => {
   const navigate = useNavigate();
   const [page, setPage] = useState<number>(1);
-  const { data } = useTmdb<MoviesResponse>(`${MOVIE_ENDPOINT}/${endpoint}`, { page }, [page, endpoint]);
-
+  const { category } = useParams<{ category: string }>();
+  const { data } = useTmdb<MoviesResponse>(`${MOVIE_ENDPOINT}/${category}`, { page }, [category, page]);
+  
   const gridData = (data?.results ?? []).map((result) => ({
     id: result.id,
     imagePath: result.poster_path,
     primaryText: result.original_title,
   }));
 
+  useEffect(() => {
+    setPage(1);
+  }, [category]);
+
   if (!data) {
     return <p className="text-center text-gray-400">Loading...</p>;
   }
-
+  
   return (
     <section className="max-w-[1200px] mx-auto p-5 space-y-5">
+      <h1 className="text-3xl font-bold mb-4">Movies</h1>
       <LinkGroup
         options={[
-          { label: "Now Playing", to: "/movies/category/now_playing" },
-          { label: "Popular", to: "/movies/category/popular" },
-          { label: "Top Rated", to: "/movies/category/top_rated" },
-          { label: "Upcoming", to: "/movies/category/upcoming" },
-        ]} />
-      {/* <h1 className="text-3xl font-bold mb-4">Now Playing</h1> */}
+          { label: 'Now Playing', to: '/movies/category/now_playing' },
+          { label: 'Popular',     to: '/movies/category/popular'     },
+          { label: 'Top Rated',   to: '/movies/category/top_rated'   },
+          { label: 'Upcoming',    to: '/movies/category/upcoming'    },
+        ]}
+      />
       <ImageGrid results={gridData} onClick={(id) => navigate(`/movie/${id}/credits`)} />
       <Pagination page={page} maxPages={data.total_pages} onClick={setPage} />
     </section>
